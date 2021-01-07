7d6eadcb644d0ae140f52fe229a414ec90e93bca
Rahul S. Shinde <rshinde@mica.edu>
2021-01-07 17:54:16 -0500

    adding in home button for project page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-
+fetch('https://extreme-ip-lookup.com/json/')
+.then( res => res.json())
+.then(response => {
+    console.log(response);
+    if (response.region != 'Pennsylvania' && response.region != 'Philadelphia'){
+      document.querySelectorAll('.movable').forEach((e) => {
+        e.remove();
+      });
+      document.body.classList.add('not_pa');    
+    } else{
+      document.body.classList.add('pa');
+    }
+ })
+ .catch((data, status) => {
+    console.log('Request failed');
+ })
