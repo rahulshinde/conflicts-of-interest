aa3c7153b84e0ace6bd25200c062ce955153e31d
Rahul S. Shinde <rshinde@mica.edu>
2021-01-07 19:39:32 -0500

    adding audio play pause

--- js/main.js
+++ js/main.js
@@ -1,24 +1,36 @@
+var audio = document.getElementById('audio_player');
 document.addEventListener('DOMContentLoaded', init);
 
 function init(){
   fetch('https://extreme-ip-lookup.com/json/')
   .then( res => res.json())
   .then(response => {
-      console.log(response);
-      if (response.region != 'Pennsylvania' && response.region != 'Philadelphia'){
-        document.querySelectorAll('.movable').forEach((e) => {
-          e.remove();
-        });
-        document.body.classList.add('not_pa');    
-      } else{
-        document.body.classList.add('pa');
-      }
-   })
-   .catch((data, status) => {
-      console.log('Request failed');
-   })
+    if (response.region != 'Pennsylvania' && response.region != 'Philadelphia'){
+      document.querySelectorAll('.movable').forEach((e) => {
+        e.remove();
+      });
+      document.body.classList.add('not_pa');    
+    } else{
+      document.body.classList.add('pa');
+    }
+  })
+  .catch((data, status) => {
+  })
 
-   
+  document.querySelectorAll('.play_button').forEach((e) => {
+    e.addEventListener('click', toggleState);
+  });
+}
+
+function toggleState(e){
+  document.querySelector('.selected').classList.remove('selected');
+  if (e.target.id == 'play'){
+    audio.play();
+  } else{
+    audio.pause();
+  }
+  console.log(e);
+  e.target.classList.add('selected');
 }
 
 
