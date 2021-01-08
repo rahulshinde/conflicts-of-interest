a93338f15fbb61f3a236eb7655852db3a82a89b1
Rahul S. Shinde <rshinde@mica.edu>
2021-01-07 19:08:54 -0500

    hover

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
-fetch('https://extreme-ip-lookup.com/json/')
-.then( res => res.json())
-.then(response => {
-    console.log(response);
-    if (response.region != 'Pennsylvania' && response.region != 'Philadelphia'){
-      document.querySelectorAll('.movable').forEach((e) => {
-        e.remove();
-      });
-      document.body.classList.add('not_pa');    
-    } else{
-      document.body.classList.add('pa');
-    }
- })
- .catch((data, status) => {
-    console.log('Request failed');
- })
+document.addEventListener('DOMContentLoaded', init);
+
+function init(){
+  fetch('https://extreme-ip-lookup.com/json/')
+  .then( res => res.json())
+  .then(response => {
+      console.log(response);
+      if (response.region != 'Pennsylvania' && response.region != 'Philadelphia'){
+        document.querySelectorAll('.movable').forEach((e) => {
+          e.remove();
+        });
+        document.body.classList.add('not_pa');    
+      } else{
+        document.body.classList.add('pa');
+      }
+   })
+   .catch((data, status) => {
+      console.log('Request failed');
+   })
+
+   
+}
+
+
